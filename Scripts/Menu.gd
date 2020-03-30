1e366dd30df206a7d4e99a1a18103d64ff0a7b47
Javier Rodriguez <profe.jars@gmail.com>
2020-03-30 01:24:56 -0300

    agregue solapas bio, inv y log. Falta implementar log

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,13 +1,33 @@
 extends CanvasLayer
 
 
+var selected_tab = null
 
-
-
+func _ready():
+	select_tab($Display/Bio)
+	$Display.hide()
+	
 
 
 func _on_MenuButton_pressed():
 	var tree = get_tree()
-	tree.paused = !tree.paused
-	$ColorRect.visible = !$ColorRect.visible 
-	$Inventory.visible = !$Inventory.visible
+	tree.paused = !tree.paused	
+	$Display.visible = !$Display.visible
+
+func select_tab(tab):
+	if tab == selected_tab:
+		return
+	if selected_tab != null:
+		selected_tab.hide()
+	selected_tab = tab
+	selected_tab.show()
+
+
+func _on_BioButton_pressed():
+	select_tab($Display/Bio)
+
+
+
+
+func _on_InventoryButton_pressed():
+	select_tab($Display/Inventory)
